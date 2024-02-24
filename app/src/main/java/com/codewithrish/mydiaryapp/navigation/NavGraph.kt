be0ecfa95fc50a73288c9553a608733e2b908bc9
rishabhkumar <rishabhkumar1024@gmail.com>
2024-02-24 21:13:22 +0530

    Auth Screen Design

--- app/src/main/java/com/codewithrish/mydiaryapp/navigation/NavGraph.kt
+++ app/src/main/java/com/codewithrish/mydiaryapp/navigation/NavGraph.kt
@@ -7,10 +7,14 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.codewithrish.mydiaryapp.presentation.screens.auth.AuthenticationScreen
 import com.codewithrish.mydiaryapp.util.Constants.WRITE_SCREEN_ARGUMENT_KEY
 
 @Composable
-fun SetupNavGraph(startDestination: String, navController: NavHostController) {
+fun SetupNavGraph(
+    startDestination: String,
+    navController: NavHostController
+) {
     NavHost(
         startDestination = startDestination,
         navController = navController
@@ -23,19 +27,24 @@ fun SetupNavGraph(startDestination: String, navController: NavHostController) {
 
 fun NavGraphBuilder.authenticationRoute() {
     composable(route = Screen.Authentication.route) {
+        AuthenticationScreen(
+            loadingState = false,
+            onButtonClicked = {
 
+            }
+        )
     }
 }
 
 fun NavGraphBuilder.homeRoute() {
-    composable(route = Screen.Authentication.route) {
+    composable(route = Screen.Home.route) {
 
     }
 }
 
 fun NavGraphBuilder.writeRoute() {
     composable(
-        route = Screen.Authentication.route,
+        route = Screen.Write.route,
         arguments = listOf(navArgument(name = WRITE_SCREEN_ARGUMENT_KEY) {
             type = NavType.StringType
             nullable = true
